71e1c675fc3cfc7f1c62c1fe768735b672c4dfdb
Edouard Proust <contact@edouardproust.dev>
2025-07-17 01:40:58 +0200

    cpp06 ex01

---
+++ cpp06/ex01/Data.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <string>
+
+struct Data {
+	std::string firstname;
+	std::string lastname;
+	int			age;
+	char		grade;
+};
+
+#endif
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Serializer.hpp"
+#include <iostream>
+
+int	main()
+{
+	Data*	d = new Data;
+	d->firstname = "Isaac";
+	d->lastname = "Newton";
+	d->age = 381;
+	d->grade = 'S';
+
+	uintptr_t	serializedData = Serializer::serialize(d);
+	Data*		res = Serializer::deserialize(serializedData);
+
+	std::cout << "firstname: " << res->firstname
+			<< "\nlastname: " << res->lastname
+			<< "\nage: " << res->age
+			<< "\ngrade: " << res->grade
+			<< std::endl;
+
+	delete d;
+	return (0);
+}
