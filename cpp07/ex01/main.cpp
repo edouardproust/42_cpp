ead0ad0346b31a26a0a256c7002cc2ed436a8426
Edouard Proust <eproust@cbr12s5.42barcelona.com>
2025-08-04 12:14:19 +0200

    update

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -23,16 +23,20 @@ int	main() {
 	// array of ints
 	int arr0[] = {0, 1, 2, 3, 4};
 	iter(arr0, 5, print); std::cout << std::endl;
+
 	// array of floats
 	float arr1[] = {0.0f, 1.1f, 2.2f, 3.3f, 4.4f};
 	iter(arr1, 5, print); std::cout << std::endl;
+
 	// array of strings
 	std::string	arr2[] = {"Hello", "World", "I", "am", "Edouard", "the", "Mighty"};
 	iter(arr2, 5, print); std::cout << std::endl;
-	// array of structs (wraping a float)
+
+	// array of structs (wraping a double)
 	Custom a, b(1.1), c(2.2), d(3.3), e(4.4);
 	Custom arr3[5] = {a, b, c, d, e};
 	iter(arr3, 5, print); std::cout << std::endl;
+
 	// vector of ints
 	std::vector<int>	vec;
 	vec.push_back(10);
