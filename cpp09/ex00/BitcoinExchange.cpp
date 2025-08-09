6d86cc3312faf9263b080ff4765e9ce290a55531
Edouard Proust <contact@edouardproust.dev>
2025-08-09 19:26:08 +0200

    ex00 finished

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,2 +1,147 @@
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(char const* dataPath, char const* inputPath)
+: _dataPath(dataPath), _inputPath(inputPath)
+{
+	_parseData();
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const& src)
+: _dataPath(src._dataPath), _inputPath(src._inputPath), _data(src._data) {}
+
+BitcoinExchange&	BitcoinExchange::BitcoinExchange::operator=(BitcoinExchange const& rhs)
+{
+	if (this != &rhs) {
+		_dataPath = rhs._dataPath;
+		_inputPath = rhs._inputPath;
+		_data = rhs._data; // Use of std::map's operator=()
+	}
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+void	BitcoinExchange::printOutput() const {
+	// Check that file exist and can be open
+	std::ifstream ifs(_inputPath);
+	if (!ifs) {
+		throw std::runtime_error("Error: invalid input file '" + std::string(_inputPath) + "'");
+	}
+	// Loop through each line of the input file
+	for (std::string line; std::getline(ifs, line);) {
+		// Skip lines without date
+		if (!_isLineWithDate(line)) {
+			continue;
+		}
+		// Split line into date and value 
+		std::string date, value;
+		if (!_splitLine(line, " | ", date, value)) {
+			std::cerr << "Error: bad input => " << line << std::endl;
+			continue;
+		}
+		// Check date validity
+		try {
+			_checkDate(date);
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+			continue;
+		}
+		// Convert value into a float + check value validity
+		float fValue= static_cast<float>(std::atof(value.c_str()));
+		try {
+			_checkInputLineValue(fValue);
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+			continue;
+		}
+		// Calculate and display result for this line
+		double res = _getResult(date, fValue);
+		std::cout << date << " => " << fValue << " = " << res << std::endl;
+	}
+};
+
+double	BitcoinExchange::_getResult(std::string const& date, double const& value) const
+{
+	Data::const_iterator current = _data.begin();
+	Data::const_iterator next = ++_data.begin();
+	while (next != _data.end()) {
+		if (current->first == date || next->first > date) {
+			return current->second * value;
+		} else if (next->first == date) {
+			return next->second * value;
+		}
+		++current;
+		++next;
+	}
+	throw std::runtime_error("Error: no result found");
+}
+
+void	BitcoinExchange::_parseData()
+{
+	std::ifstream ifs(_dataPath);
+	if (!ifs) {
+		throw std::runtime_error("Error: invalid data file '" + std::string(_dataPath) + "'");
+	}
+	for (std::string line; std::getline(ifs, line);) {
+		std::string date, value;
+		if (!_splitLine(line, ",", date, value)) continue; // skip lines that cannot be split
+		try {
+			_checkDate(date);
+		} catch (...) {
+			continue; // skip lines with invalid date format
+		}
+		_data[date] = std::atof(value.c_str());
+	}
+}
+
+// Static functions
+
+bool	BitcoinExchange::_isLineWithDate(std::string const& str)
+{
+	if (str.empty()) {
+		return false;
+	}
+	char* p;
+	strtod(str.c_str(), &p);
+	return (*p == '-');
+}
+
+bool	BitcoinExchange::_splitLine(std::string const& line, std::string const& sep,
+std::string& lhs, std::string& rhs)
+{
+	if (!_isLineWithDate(line)) return false; // line has no date
+	size_t pos = line.find(sep);
+	if (pos == line.npos) return false; // line has 1 column
+	lhs = line.substr(0, pos);
+	rhs = line.substr(pos + sep.size(), line.size());
+	return true;
+}
+
+void	BitcoinExchange::_checkDate(std::string const& s)
+{
+	size_t	dash1Pos = s.find("-");
+	size_t	dash2Pos = s.find("-", dash1Pos + 1);
+	if (dash1Pos == s.npos || dash2Pos == s.npos) {	
+		throw std::runtime_error("Error: invalid date format => " + s + " (should be YYYY-MM-DD)");
+	}
+	int year = std::atoi(s.substr(0, dash1Pos).c_str());
+	int month = std::atoi(s.substr(dash1Pos + 1, dash2Pos).c_str());
+	int day = std::atoi(s.substr(dash2Pos + 1, s.size()).c_str());
+	if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31
+		|| ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+		|| (month == 2 && day > 29)) {
+			throw std::runtime_error("Error: invalid date => " + s);
+		}
+	}
+
+void	BitcoinExchange::_checkInputLineValue(double const& value)
+{
+	if (value < 0) {
+		throw std::runtime_error("Error: not a positive number");
+	} else if (value > 1000){
+		throw std::runtime_error("Error: too large number");
+	}
+}
+
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -2,108 +2,40 @@
 #define BITCOIN_EXCHANGE_HPP
 
 #include <string> // std::string, std::getline()
-#include <map> // std::map<...>
+#include <map> // std::map<>
 #include <iostream> // std::cout|cerr|endl
 #include <fstream> // std:ifstream()
 #include <stdexcept> // std::runtime_error
 #include <stdlib.h> // strtod()
 #include <cstdlib> // std::atof()
 
-#include <iomanip> // TODO remove after testing
-
 class BitcoinExchange
 {
-	struct FileData {
-		double			value;
-		unsigned int	year;
-		unsigned int	month;
-		unsigned int	day;
-	};
+	typedef std::map<std::string, double>	Data;
 
-	typedef std::map<std::string, FileData>	BtcFile;
-
-	char const*						_dataPath;
-	char const*						_inputPath;
-	BtcFile							_data;
+	char const*	_dataPath;
+	char const*	_inputPath;
+	Data		_data;
 	
-	BitcoinExchange() {}
+	BitcoinExchange();
 
+	void			_parseData();
+	double			_getResult(std::string const& date, double const& value) const;
 
-	bool	_isDateLine(std::string const& str) {
-		if (str.empty()) {
-			return false;
-		}
-		char* p;
-		strtod(str.c_str(), &p);
-		return (*p == '-');
-	}
-
-	bool	_parseDate(std::string const& s, FileData& fd) {
-		size_t	dash1Pos = s.find("-");
-		size_t	dash2Pos = s.find("-", dash1Pos + 1);
-		if (dash1Pos == s.npos || dash2Pos == s.npos) {
-			return false;
-		}
-		fd.year = std::atoi(s.substr(0, dash1Pos).c_str());
-		fd.month = std::atoi(s.substr(dash1Pos + 1, dash2Pos).c_str());
-		fd.day = std::atoi(s.substr(dash2Pos + 1, s.size()).c_str());
-		return true;
-	}
-
-	void	_parseFile(std::ifstream& ifs, std::string const& sep, BtcFile& btcFile) {
-		for (std::string line; std::getline(ifs, line);) {
-			if (!_isDateLine(line)) continue; // skip lines without date
-			size_t pos = line.find(sep);
-			if (pos == line.npos) continue; // skip lines with only 1 column
-			std::string lhs = line.substr(0, pos);
-			std::string rhs = line.substr(pos + 1, line.size());
-			FileData fd;
-			if (!_parseDate(lhs, fd)) continue; // skip lines with invalid date format
-			fd.value = std::atof(rhs.c_str());
-			btcFile[lhs] = fd;
-		}
-	}
-
-	void	_parseFiles() {
-		// open files
-		std::ifstream dataIfs(_dataPath);
-		if (!dataIfs) {
-			throw std::runtime_error("Invalid data file '" + std::string(_dataPath) + "'");
-		}
-		std::ifstream inputIfs(_inputPath);
-		if (!inputIfs)  {
-			throw std::runtime_error(std::string("Invalid input file '") + std::string(_inputPath) + "'");
-		}
-		// parse content
-		_parseFile(dataIfs, ",", _data);
-		for (BtcFile::const_iterator it = _data.begin(); it != _data.end(); ++it) {
-			std::cout << it->first << ": " << it->second.year << " " << it->second.month << " " << it->second.day << " " << std::fixed << std::setprecision(2) << it->second.value << std::endl;
-		}
-	}
+	static bool		_isLineWithDate(std::string const& str);
+	static bool		_splitLine(std::string const& line, std::string const& sep,
+						std::string& lhs, std::string& rhs);
+	static void		_checkDate(std::string const& s);
+	static void		_checkInputLineValue(double const& value);
 
 	public:
 
-		BitcoinExchange(char const* dataPath, char const* inputPath):
-			_dataPath(dataPath), _inputPath(inputPath) {
-			_parseFiles();
-		}
-		BitcoinExchange(BitcoinExchange const& src):
-			_dataPath(src._dataPath), _inputPath(src._inputPath) {
-			_parseFiles();
-		}
-		BitcoinExchange&	operator=(BitcoinExchange const& rhs) {
-			if (this != &rhs) {
-				// TODO
-			}
-			return *this;
-		}
-		~BitcoinExchange() {
-			// TODO
-		}
+		BitcoinExchange(char const* dataPath, char const* inputPath);
+		BitcoinExchange(BitcoinExchange const& src);
+		BitcoinExchange&	operator=(BitcoinExchange const& rhs);
+		~BitcoinExchange();
 
-		void	printOutput() const {
-			std::cout << "Output here" << std::endl; //TODO
-		}
+		void	printOutput() const;
 };
 
 #endif
