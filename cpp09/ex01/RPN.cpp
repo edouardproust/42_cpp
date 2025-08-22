b624fb0206d950f39c866f2203b4d510d597fa2a
Edouard Proust <contact@edouardproust.dev>
2025-08-22 18:54:55 +0200

    ex01 finished

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -1,2 +1,82 @@
 #include "RPN.hpp"
 
+RPN::RPN(std::string const& input) {
+	_calculate(input);
+}
+
+RPN::~RPN() {}
+
+int const&	RPN::getResult() const {
+	if (_stack.size() != 1) {
+		throw std::runtime_error("unprocessed operand(s)");
+	}
+	return _stack.top();
+}
+
+void	RPN::_calculate(std::string const& expr)
+{
+	// Check chars validity
+	if (expr.find_first_not_of("0123456789 +-*/") != std::string::npos) {
+		throw std::runtime_error("invalid char found");
+	}
+	// Extract tokens and process them
+	std::istringstream ss(expr);
+	for (std::string token; ss >> token;) {
+	//for (std::string token; std::getline(ss, token, ' ');) {
+	//	if (token.size() == 0) continue;
+		if (token.size() == 1 && token.find_first_of("+-/*") != std::string::npos) {
+			_pushOperatorResult(token);
+		} else {
+			_pushOperand(token);
+		}
+	}
+}
+
+void	RPN::_pushOperand(std::string const& operand)
+{
+	char* endptr;
+	long n = std::strtol(operand.c_str(), &endptr, 10);
+	if (*endptr != '\0') {
+		throw std::runtime_error("invalid operand");
+	}
+	if (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max()) {	
+		throw std::runtime_error("operand oveflow");
+	}
+	_stack.push(static_cast<int>(n));
+}
+
+void	RPN::_pushOperatorResult(std::string const& op)
+{
+	// Operator needs 2 operands to be possible
+	if (_stack.size() < 2) {
+		throw std::runtime_error("not enough operands before operator");
+	}
+	// Get the 2 top operands
+	int second = _stack.top();
+	_stack.pop();
+	int first = _stack.top();
+	_stack.pop();
+	// Do the calculation
+	long res;
+	if (op == "+") {
+		res = static_cast<long>(first) + second;
+	} else if (op == "-") {
+		res = static_cast<long>(first) - second;
+	} else if (op == "/") {
+		if (second == 0) {
+			throw std::runtime_error("division by 0");
+		}
+		res = static_cast<long>(first) / second;
+	} else if (op == "*") {
+		res = static_cast<long>(first) * second;
+	} else {
+		throw std::runtime_error("invalid operator token");
+	}
+	// Check result overflow
+	if (res < std::numeric_limits<int>::min() || res > std::numeric_limits<int>::max()) {
+		throw std::runtime_error("intermediary result overflow");
+	}
+	// Push result to stack
+	_stack.push(static_cast<int>(res));
+}
+
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -1,16 +1,44 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
+#include <string> // std::string
+#include <iostream> // std::cout|cerr|endl
+#include <sstream> // std::istringstream
+#include <stack> // std::stack<>
+#include <cstdlib> // std::strtol()
+#include <limits> // std::numeric_limits<>::min()|max()
+#include <stdexcept> // std::runtime_error()
+
+/**
+ * RPN error cases:
+ * - on parsing:
+ *  meet an char other than [0-9] + - / * or space
+ *  meet an operand >= 10
+ * - on caclulation
+ *  meet an operator + - / * but there is less than 2 operands in stack
+ *  division by 0
+ *  int overflow
+ * - on reaching the end of expression
+ *  number of operands in stack is not 1
+ */
 class RPN
 {
-/*
-	public:
+	std::stack<int>	_stack;
 
-		RPN();
-		RPN(RPN const&);
-		RPN&	operator=(RPN const&);
+	RPN();
+	RPN(RPN const&);
+	RPN&	operator=(RPN const&);
+
+	void	_calculate (std::string const&);
+	void	_pushOperand(std::string const&);
+	void	_pushOperatorResult(std::string const&);
+
+	public:
+	
+		RPN(std::string const&);
 		~RPN();
-*/
+
+		int const&	getResult() const;
 };
 
 #endif
