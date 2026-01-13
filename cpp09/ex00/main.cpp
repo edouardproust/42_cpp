a2be5b25bdf926476647704287bb29f391f850bc
Edouard Proust <contact@edouardproust.dev>
2026-01-13 16:50:23 +0100

    cleanup

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -3,7 +3,7 @@
 int	main(int ac, char **av)
 {
 	if (ac != 2) {
-		std::cerr << "Usage: ./btn <inputFile>" << std::endl;
+		std::cerr << "Usage: ./btn <file_path>" << std::endl;
 		return 2;
 	}
 	BitcoinExchange btcEx("data.csv", av[1]);
